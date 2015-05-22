1196c658b08aa53a0b29cfaf219385173eb8a943
Moritz Schmidt <fusselwurm@gmail.com>
2015-05-22 19:44:37 +0200

    initial commit (structure copied from ar3play-addon

---
+++ tfar_autoswitch/init.hpp
@@ -0,0 +1,5 @@
+//#ifndef DEBUG_MODE_FULL
+//	#define DEBUG_MODE_FULL
+//#endif
+
+#include "\x\cba\addons\main\script_macros_common.hpp"
---
+++ tfar_autoswitch/init.sqf
@@ -0,0 +1,4 @@
+#include "init.hpp"
+
+waitUntil { isDedicated || isServer; };
+
