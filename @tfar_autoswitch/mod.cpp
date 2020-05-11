9e41dd559ea5310915c7a56367ec93109c8a7deb
Moritz Schmidt <mo.schmidt@rebuy.com>
2020-05-11 11:11:05 +0200

    clenup

--- @tfar_autoswitch/mod.cpp
+++
@@ -1,4 +0,0 @@
-name = "tfar_autoswitch";
-actionName = "Website";
-action = "https://github.com/gruppe-adler/tfar_autoswitch";
-description = "Arma3 automatic channel switching based on game server port.";
---
+++ addons/main/CfgEventHandlers.hpp
@@ -0,0 +1,7 @@
+class Extended_PostInit_EventHandlers
+{
+    class ADDON
+    {
+        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+    };
+};
